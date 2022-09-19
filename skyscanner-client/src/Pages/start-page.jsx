343dbad9e1d51aeeac391eb2595eb11349693f60
SergeyZup <sergey@zup.ee>
2022-09-19 18:37:29 +0300

    add new components

--- skyscanner-client/src/Pages/start-page.jsx
+++ skyscanner-client/src/Pages/start-page.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Grid } from "semantic-ui-react";
+import { Card, Grid, Icon } from "semantic-ui-react";
 import styles from "./startpage.module.scss";
 
 import Filter from "../Common/filter";
 import AccordionS from "../Common/accordion";
-
 
 const StartPage = () => {
 	const options = [
@@ -27,9 +26,8 @@ const StartPage = () => {
 
 	return (
 		<Grid>
-
 			<Grid.Row className={styles.row1}>
-				<Grid.Column width={16} className={styles.backImg} >
+				<Grid.Column width={16} className={styles.backImg}>
 					<Grid>
 						<Grid.Row>
 							<Grid.Column width={4}></Grid.Column>
@@ -41,11 +39,10 @@ const StartPage = () => {
 							<Grid.Column width={4}></Grid.Column>
 						</Grid.Row>
 
-						<Grid.Row className={styles.filterRow}  centered columns={5}>
+						<Grid.Row className={styles.filterRow} centered columns={5}>
 							<Grid.Column width={15}>
-								<Filter/>
+								<Filter />
 							</Grid.Column>
-							
 						</Grid.Row>
 					</Grid>
 				</Grid.Column>
@@ -60,12 +57,13 @@ const StartPage = () => {
 				))}
 			</Grid.Row>
 
-			<Grid.Row className={styles.row3}><AccordionS/></Grid.Row>
+			<Grid.Row className={styles.row3}>
+				<AccordionS />
+			</Grid.Row>
 
 			<Grid.Row centered className={styles.row4}>
-				<Grid.Column width={10} className={styles.row3Column1}>
+				<Grid.Column width={10}>
 					<Grid>
-						
 						<Grid.Row centered>
 							<Grid.Column width={15}>
 								<div className={styles.bestFlightHeader}>
@@ -86,7 +84,6 @@ const StartPage = () => {
 						</Grid.Row>
 
 						<Grid.Row centered columns={3}>
-
 							<Grid.Column width={5}>
 								<div className={styles.img2row}>
 									<img
@@ -148,10 +145,119 @@ const StartPage = () => {
 				</Grid.Column>
 			</Grid.Row>
 
+			<Grid.Row className={styles.row5}>
+				<Grid.Column width={16}>
+					<Grid>
+						<Grid.Row className={styles.row5p1}>
+							<Grid.Column>
+								<span className={styles.headRow5p1}>
+									Plan your journey with peace of mind
+								</span>
+								<span className={styles.subRow5p1}>
+									We've made it our mission to help you travel with confidence
+									and make your journey as smooth as possible.
+								</span>
+							</Grid.Column>
+						</Grid.Row>
 
-			<Grid.Row>
-				
+						<Grid.Row className={styles.row5p2} columns={2}>
+							<Grid.Column width={8} className={styles.row5p2Col1}>
+								<div className={styles.headRow5p2}>
+									<Icon name="bell" color="blue" />
+									<span>Find flexible flight deals</span>
+								</div>
+								<div className={styles.subRow5p2v1}>
+									<span>
+										Explore flexible plane ticket deals so you won't lose out if
+										your flight is changed or cance
+									</span>
+								</div>
+							</Grid.Column>
+							<Grid.Column width={8} className={styles.row5p2Col1}>
+								<div className={styles.head2Row5p2}>
+									<Icon name="certificate" color="blue" />
+									<span>Add hotels and car rentals</span>
+								</div>
+								<div className={styles.subRow5p2v1}>
+									<span>
+										Plan your journey with hotels and car rentals, and keep your
+										bookings all in one place
+									</span>
+								</div>
+							</Grid.Column>
+						</Grid.Row>
+
+						<Grid.Row className={styles.row5p3}>
+							<Grid.Column className={styles.card}>
+								<Grid>
+									<Grid.Row columns={2}>
+										<Grid.Column width={10} className={styles.cardForm}>
+											<div className={styles.cardHeader}>
+												<span>Don't miss a single flight deal </span>
+											</div>
+											<div className={styles.cardSubText}>
+												<span>
+													Get cheap flights and unmissable travel deals sent
+													straight to your inbox.
+												</span>
+											</div>
+											<div>
+												<input className={styles.cardInput} placeholder="Enter your email address" />
+												<button>Sign up</button>
+											</div>
+											<div>
+												<span>
+													You can unsubscribe at any time. Read our Privacy
+													Policy for more info.
+												</span>
+											</div>
+										</Grid.Column>
+										<Grid.Column width={6}>
+											<img
+												className={styles.cardImg}
+												src="https://content.skyscnr.com/m/6ea151fbaf83db9e/original/Emailcapture.png"
+											/>
+										</Grid.Column>
+									</Grid.Row>
+								</Grid>
+							</Grid.Column>
+						</Grid.Row>
+						{/* <Card className={styles.card}>
+						<Grid.Row columns={2} className={styles.row5p3}>
+							<Grid.Column width={10} className={styles.row5p3col1}>
+								<div className={styles.headRow5p3}>
+									<span>Don't miss a single flight deal </span>
+								</div>
+								<div className={styles.subRow5p3}>
+									<span>
+										Get cheap flights and unmissable travel deals sent straight
+										to your inbox.
+									</span>
+								</div>
+								<div>
+									<input placeholder="Enter your email address" />
+									<button>Sign up</button>
+								</div>
+								<div>
+									<span>
+										You can unsubscribe at any time. Read our Privacy Policy for
+										more info.
+									</span>
+								</div>
+							</Grid.Column>
+
+							<Grid.Column
+								width={3}
+								className={styles.row5p3col1}
+							></Grid.Column>
+						</Grid.Row>
+						</Card> */}
+					</Grid>
+				</Grid.Column>
 			</Grid.Row>
+
+			<Grid.Row></Grid.Row>
+			<Grid.Row></Grid.Row>
 		</Grid>
 	);
 };
