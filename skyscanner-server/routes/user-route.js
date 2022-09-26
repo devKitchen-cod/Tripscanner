2a6e1143c8cf70c9737094f57f7aa65f7e89ceed
SergeyZup <sergey@zup.ee>
2022-09-26 17:02:08 +0300

    fis

--- skyscanner-server/routes/user-route.js
+++ skyscanner-server/routes/user-route.js
@@ -6,9 +6,9 @@ const authController = require('../controller/authController')
 router.post('/auth-user-create', authController.createUser)
 router.post('/auth-user-update', authController.updateUser)
 router.post('/auth-user-login', authController.loginUser)
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+/* GET users listing. */
+router.get('/', function(req, res ) {
+  res.send('respond with a resource');
+});
 
 module.exports = router;
