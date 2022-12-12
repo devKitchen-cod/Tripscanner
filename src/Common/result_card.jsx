604f9395207558f44a11a704f0c5044284d16bda
SergeyZup <sergey@zup.ee>
2022-12-12 12:49:34 +0200

    fix

--- src/Common/result_card.jsx
+++ src/Common/result_card.jsx
@@ -5,15 +5,26 @@ import { Grid } from 'semantic-ui-react';
 const ResCard = () => {
   return (
     <Grid>
-      <Grid.Row className={styles.row1}>
-        <Grid.Column width={16}>
-          
+      <Grid.Row centered className={styles.row1}>
+        <Grid.Column className={styles.timeFlight} width={10}>          
           <Grid>
-            <Grid.Row>
+            <Grid.Row className={styles.row2}>
+              <Grid.Column className={styles.logoflight} width={6}>
+                
+                <img className={styles.logoFlight} alt="Austrian Airlines" src="//www.skyscanner.net/images/airlines/small/OS.png"></img>
+              </Grid.Column>
 
+              <Grid.Column width={10}>
+                ewq
+              </Grid.Column>
             </Grid.Row>
+            <Grid.Row>dsa</Grid.Row>
           </Grid>
           
+        </Grid.Column>
+
+        <Grid.Column className={styles.priceFlight} width={6}>
+          dsa
         </Grid.Column>
       </Grid.Row>
     </Grid>
--- src/Pages/flights-search-page.jsx
+++ src/Pages/flights-search-page.jsx
@@ -11,18 +11,14 @@ import { useEffect } from "react";
 import { TransitionablePortal } from "semantic-ui-react";
 
 import { options } from "../mock/mockdata";
-import { Airlines, testResSerch } from "../mock/mockdata";
+import {   testResSerch } from "../mock/mockdata";
 import { radioBtn } from "../mock/mockdata";
 import ResCard from "../Common/result_card";
 import FilterSearch from "../Common/filter-flight--search";
 
 const FlightSearch = () => {
 	const [open, setOpen] = useState(true);
-	const [openStops, setOpenStops] = useState(true);
-	const [openDepart, setOpenDepart] = useState(true);
-	const [trip, setTrip] = useState(true);
-	const [airlines, setAirlines] = useState(true);
-	const [greener, setGreener] = useState(true);
+	
 
 	const obj = {
 		from: "London (Any)",
@@ -64,6 +60,7 @@ const FlightSearch = () => {
 						) : (
 							<Grid.Row className={styles.header1}>
 								<Grid.Column width={16} onClick={() => setOpen(true)}>
+
 									<Grid>
 										<Grid.Row className={styles.header12}>
 											<Grid.Column width={8}>
@@ -233,12 +230,17 @@ const FlightSearch = () => {
 							</Grid.Column>
 
 						</Grid.Row>
-
-					</Grid>
-
-					{testResSerch.map(() => (
+						<Grid.Row centered>
+							<Grid.Column width={15}>
+							{testResSerch.map(() => (
 						<ResCard />
 					))}
+							</Grid.Column>
+						</Grid.Row>
+					</Grid>
+
+					
+
 				</Grid.Column>
 			</Grid.Row>
 		</Grid>
@@ -247,139 +249,3 @@ const FlightSearch = () => {
 
 export default FlightSearch;
 
-// const [value, setValue] = useState([50]);
-
-// const transitions = [
-// 	"browse",
-// 	"browse right",
-// 	"drop",
-// 	"fade",
-// 	"fade up",
-// 	"fade down",
-// 	"fade left",
-// 	"fade right",
-// 	"fly up",
-// 	"fly down",
-// 	"fly left",
-// 	"fly right",
-// 	"horizontal flip",
-// 	"vertical flip",
-// 	"scale",
-// 	"slide up",
-// 	"slide down",
-// 	"slide left",
-// 	"slide right",
-// 	"swing up",
-// 	"swing down",
-// 	"swing left",
-// 	"swing right",
-// 	"zoom",
-// ];
-// const options = transitions.map((name) => ({
-// 	key: name,
-// 	text: name,
-// 	value: name,
-// }));
-{
-	/* <Accordion fluid>
-<Accordion.Title
-	className={styles.titleStops}
-	onClick={() => setOpenStops(!openStops)}
->
-	Stops
-	<Icon name="dropdown" />
-</Accordion.Title>
-
-<Accordion.Content active={openStops}>
-	<div className={styles.stopsFilter}>
-		<label className={styles.label1}>
-			<input type={"checkbox"} />
-			<span>Non-stop</span>
-		</label>
-
-		<label className={styles.label1}>
-			<input type={"checkbox"} />
-			<span>1 stop</span>
-		</label>
-
-		<label className={styles.label1}>
-			<input type={"checkbox"} />
-			<span>2+ stops</span>
-		</label>
-	</div>
-</Accordion.Content>
-
-<Accordion.Title
-	className={styles.titleDepartureTimes}
-	onClick={() => setOpenDepart(!openDepart)}
->
-	Departure times
-	<Icon name="dropdown" />
-</Accordion.Title>
-
-<Accordion.Content active={openDepart}>
-	<div>
-		<p>Outbound</p>
-		<span>5 AM - 14 PM</span>
-		<div>
-			<input type={"range"} id="min" step={1} />
-		</div>
-	</div>
-
-	<div>
-		<p>Return</p>
-		<span>5 AM - 14 PM</span>
-		<div>
-			<input type={"range"} id="min" step={1} />
-		</div>
-	</div>
-</Accordion.Content>
-
-<Accordion.Title onClick={() => setTrip(!trip)}>
-	Trip Duration
-	<Icon name="dropdown" />
-</Accordion.Title>
-<Accordion.Content active={trip}>
-	<div>
-		<p>Return</p>
-		<span>5 AM - 14 PM</span>
-		<div>
-			<input type={"range"} id="min" step={1} />
-		</div>
-	</div>
-</Accordion.Content>
-
-<Accordion.Title onClick={() => setAirlines(!airlines)}>
-	Airlines
-	<Icon name="dropdown" />
-</Accordion.Title>
-
-<Accordion.Content active={airlines}>
-	<div>
-		<span>Select all</span>
-		<span>Clear all</span>
-	</div>
-	<div>
-		<span>Star Alliance</span>
-		<span>SkyTeam</span>
-	</div>
-	{Airlines.map((item) => (
-		<div>
-			<input type={"checkbox"} />
-			<span>{item.name}</span>
-			<span>from {item.price}</span>
-		</div>
-	))}
-</Accordion.Content>
-
-<Accordion.Title onClick={() => setGreener(!greener)}>
-	Greener flights
-	<Icon name="dropdown" />
-</Accordion.Title>
-<Accordion.Content active={greener}>
-	<input type={"checkbox"} />
-	<span>Only show flights with lower CO₂ emissions</span>
-</Accordion.Content>
-
-</Accordion> */
-}
