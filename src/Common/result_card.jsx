12eab07892099e14854ebb41852d7a6d37809840
devKitchen-code <smagometov649@gmail.com>
2022-12-13 09:10:37 +0200

    fix res card

--- src/Common/result_card.jsx
+++ src/Common/result_card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./styles/result_card.module.scss";
-import { Grid } from "semantic-ui-react";
+import { Grid, Icon } from "semantic-ui-react";
 import flightSvg from "../static/flight.svg";
 
 const ResCard = () => {
@@ -10,7 +10,7 @@ const ResCard = () => {
 				<Grid.Column className={styles.timeFlight} width={10}>
 					<Grid>
 						<Grid.Row className={styles.row2}>
-							<Grid.Column className={styles.logoflight} width={6}>
+							<Grid.Column className={styles.logoflight} width={4}>
 								<img
 									className={styles.logoFlight}
 									alt="Austrian Airlines"
@@ -18,25 +18,43 @@ const ResCard = () => {
 								></img>
 							</Grid.Column>
 
-							<Grid.Column className={styles.timeTo} width={10}>
+							<Grid.Column className={styles.timeTo} width={12}>
 								<span className={styles.flightTo}>12:21</span>
-                <div className={styles.divider} /> 
-								<img className={styles.flightsvg} src={flightSvg} />
+
+								<div className={styles.f}>
+									<div className={styles.divider}>
+										<span className={styles.time_flight}>11:55</span>
+									</div>
+									<div className={styles.flightsvg}>
+										<img src={flightSvg} />
+									</div>
+								</div>
+
 								<span className={styles.flightFrom}>14:23</span>
 							</Grid.Column>
 						</Grid.Row>
 
 						<Grid.Row className={styles.row3}>
-							<Grid.Column className={styles.logoflight} width={6}>
+							<Grid.Column className={styles.logoflight} width={4}>
 								<img
 									className={styles.logoFlight}
 									alt="Austrian Airlines"
 									src="//www.skyscanner.net/images/airlines/small/OS.png"
 								></img>
 							</Grid.Column>
-							<Grid.Column className={styles.timeFrom} width={10}>
+
+							<Grid.Column className={styles.timeFrom} width={12}>
 								<span className={styles.flightTo}>12:21</span>
-								<img className={styles.flightsvg1} src={flightSvg}/>
+
+								<div className={styles.f}>
+									<div className={styles.divider}>
+										<span className={styles.time_flight}>11:55</span>
+									</div>
+									<div className={styles.flightsvg1}>
+										<img src={flightSvg} />
+									</div>
+								</div>
+
 								<span className={styles.flightFrom}>14:23</span>
 							</Grid.Column>
 						</Grid.Row>
@@ -44,7 +62,14 @@ const ResCard = () => {
 				</Grid.Column>
 
 				<Grid.Column className={styles.priceFlight} width={6}>
-					dsa
+					<div className={styles.price}>
+						<span className={styles.countPrice}>4 deals from</span>
+						<span className={styles.coastPrice}>7.500</span>
+					</div>
+					<button className={styles.buyFlight}>
+						<span>Select</span>
+						<Icon name="arrow right" color="green" />
+					</button>
 				</Grid.Column>
 			</Grid.Row>
 		</Grid>
