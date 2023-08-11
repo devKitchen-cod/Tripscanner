ab9d04a1fc40e3d1859a0720adbc9a129abe81f4
devKitchen-cod <smagometov649@gmail.com>
2023-08-11 13:37:06 +0300

    fix

--- src/Common/flight-durance.jsx
+++ src/Common/flight-durance.jsx
@@ -9,8 +9,9 @@ const FlightDurance = (obj1) => {
   const obj = obj1.obj;
   // const config = useC
   const springs = useSpring({
-    from: { x: 0, opacity: 0, y: 0  },
-    to: { x: 130, opacity: 1, y: 2 },
+    from: { x:0, opacity: 0, y: 5,transform: `scale(0.5)` },
+    to: { x: 130, opacity: 1, y: 5,transform: `scale(1.5)`  },
+    // transform: `scale(1)`,
     // scale: [1, 5, 10],
     config:{
       mass: 2,
